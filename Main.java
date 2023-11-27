3f10678e724a5c6fe36fc942e10d1558b0e6868e
Nujitha Wickramasurendra <nwickramasurendra@gmail.com>
2023-11-26 22:50:32 -0500

    Add files via upload

--- Main.java
+++ Main.java
@@ -1,17 +1,13 @@
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
-import java.util.Objects;
+import java.util.*;
+
+// TODO: equivalence testing
 
 public class Main {
 
+    HashMap<String, Integer> distancesMap = new HashMap<>();
+
     public void backtrack(String[] code, List<String>[] candidates, int level, int M, int d) {
         if (level >= M) {
-//            StringBuilder result = new StringBuilder();
-//            for (int i = code.length - 1; i >= 0; i--) {
-//                result.append(code[i]).append(" ");
-//            }
-//            System.out.println(result.toString());
             return;
         }
 
@@ -20,13 +16,8 @@ public class Main {
                 code[level] = v;
                 candidates[level + 1] = new ArrayList<>();
                 for (String w : candidates[level]) {
-                    int ham = hammingDistance(Long.parseLong(w), Long.parseLong(v));
-                    if (ham >= d) {
+                    if (distancesMap.get(w+v) >= d) {
                         candidates[level + 1].add(w);
-                    } else {
-                        if (calculateDistance(w, v) >= d) {
-                            candidates[level + 1].add(w);
-                        }
                     }
                 }
                 backtrack(code, candidates, level + 1, M, d);
@@ -35,10 +26,6 @@ public class Main {
             System.out.println("Invalid bound: Value M needs to be raised" + exception);
             System.exit(0);
         }
-    }
-
-    private int hammingDistance(long num1, long num2) {
-        return Long.bitCount(num1 ^ num2);
     }
 
     private List<String> generateAllCodewords(int n, int M, int q) {
@@ -59,7 +46,7 @@ public class Main {
         }
     }
 
-    private int calculateDistance(String s1, String s2) {
+    private int calculateEditDistance(String s1, String s2) {
         int m = s1.length();
         int n = s2.length();
 
@@ -82,27 +69,43 @@ public class Main {
         return dp[m][n];
     }
 
+    private void computeDistances(List<String> codewords){
+        for (String codeword1 : codewords) {
+            for (String codeword2 : codewords) {
+                int dist = calculateEditDistance(codeword1, codeword2);
+                String key = codeword1 + codeword2;
+                distancesMap.put(key, dist);
+            }
+        }
+    }
+
     public static void main(String[] args) {
         Main test = new Main();
 
-        int n = 8;
-        int d = 5;
+        int n = 3;
+        int d = 2;
         int q = 2;
-        int M = 4;
+        int M = 2;
 
         String[] code = new String[M];
         List<String>[] candidates = new List[M + 1];
 
-        candidates[0] = test.generateAllCodewords(n, M, q);
+        List<String> generatedCodewords = test.generateAllCodewords(n, M, q);
+        candidates[0] = generatedCodewords;
+
+        test.computeDistances(generatedCodewords);
 
         test.backtrack(code, candidates, 0, M, d);
 
         if (Arrays.asList(code).contains(null)) {
             int counter = (int) Arrays.stream(code).filter(Objects::nonNull).count();
-            System.out.println("(" + n + ", " + d + ")_" + q + " code with more than "
+            System.out.println("(" + n + ", " + d + ")" + q + " code with more than "
                     + counter + " codewords cannot exist");
         } else {
             System.out.println("A valid code exists: ");
+            for (int i = code.length-1; i >= 0; i--) {
+                System.out.print(code[i] + " ");
+            }
         }
     }
 }
